72d8b3c67fb2dba594c3ffa5e2e6cd7f0a4b06fb
Samantha Cassity <cassityclaire@gmail.com>
2018-06-05 03:24:06 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,12 @@
+def about_me
+  puts "My name is Sam"
+  puts "I grew up in Phoenix"
+  puts "I'm 16 years old"
+  puts "My favorite food is chocolate"
+end
+#puts "How are you feeling?"
+#answer = gets.chomp
+#puts "So, you're feeling #{answer}"
+puts "How old are you?"
+age = gets.chomp
+puts "So, you're #{age} years old"
